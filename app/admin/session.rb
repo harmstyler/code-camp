fd5b611b6faec294a86c5fa094f1aaff62019845
Tyler Harms <tyler.harms@gmail.com>
2013-09-01 17:42:31 -0500

    changes session room to use radio buttons rather than select box.

--- app/admin/session.rb
+++ app/admin/session.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Session do
+  form do |f|
+    f.inputs "Session Details" do
+      f.input :title
+      f.input :abstract
+      f.input :speaker
+      f.input :session_time
+      f.input :session_room, :as => :radio
+    end
+    f.actions
+  end
   controller do
     def permitted_params
       params.permit(:session => [:speaker_id, :title, :abstract, :session_time_id, :session_room_id])
