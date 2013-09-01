acf847fb969b6c1ece121479e528471df06c0abd
Tyler Harms <tyler.harms@gmail.com>
2013-09-01 14:30:26 -0500

    makes the admin session times work

--- app/admin/session.rb
+++ app/admin/session.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Session do
   controller do
     def permitted_params
-      params.permit(:session => [:speaker_id, :title, :abstract])
+      params.permit(:session => [:speaker_id, :title, :abstract, :session_time_id])
     end
   end
 end
