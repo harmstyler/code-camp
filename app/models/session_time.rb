9d4af4861e99dac18aa43666370934216369944b
Tyler Harms <tyler.harms@gmail.com>
2013-09-01 20:24:47 -0500

    Adds to_s definitions for content showing up in the admin incorrectly

--- app/models/session_time.rb
+++ app/models/session_time.rb
@@ -1,3 +1,6 @@
 class SessionTime < ActiveRecord::Base
     validates_presence_of :time
+    def to_s
+      time.strftime("%l:%M %p")
+    end
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,3 +1,9 @@
 class Speaker < ActiveRecord::Base
   validates_presence_of :firstname, :lastname, :email
+  def fullname
+    "#{firstname} #{lastname}"
+  end
+  def to_s
+    fullname
+  end
 end
