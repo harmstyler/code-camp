9c08e0804cd29d9c82706201a1c20073910b67b6
Tyler Harms <tyler.harms@gmail.com>
2013-09-15 09:31:04 -0500

    fixes tests on subscriber controller
    (removed tests for controller functions that were removed)

--- test/controllers/subscribers_controller_test.rb
+++ test/controllers/subscribers_controller_test.rb
@@ -16,34 +16,4 @@ class SubscribersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create subscriber" do
-    assert_difference('Subscriber.count') do
-      post :create, subscriber: {  }
-    end
-
-    assert_redirected_to subscriber_path(assigns(:subscriber))
-  end
-
-  test "should show subscriber" do
-    get :show, id: @subscriber
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @subscriber
-    assert_response :success
-  end
-
-  test "should update subscriber" do
-    patch :update, id: @subscriber, subscriber: {  }
-    assert_redirected_to subscriber_path(assigns(:subscriber))
-  end
-
-  test "should destroy subscriber" do
-    assert_difference('Subscriber.count', -1) do
-      delete :destroy, id: @subscriber
-    end
-
-    assert_redirected_to subscribers_path
-  end
 end
