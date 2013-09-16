02365edd833deb5ac1d329110354bc8c9ea2f1b5
Tyler Harms <tyler.harms@gmail.com>
2013-09-16 09:10:18 -0500

    fixes the action mailer domain

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['POSTMARK_SMTP_SERVER'],
   :user_name      => ENV['POSTMARK_API_KEY'],
   :password       => ENV['POSTMARK_API_KEY'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'rocky-peak-3649.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
