6f263ef068be30b89b31370fbe12c1eb944e2248
Tyler Harms <tyler.harms@gmail.com>
2013-09-26 20:25:44 -0500

    Adds memecache store for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Codecamp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.cache_store = :dalli_store
 end
