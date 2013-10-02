f5df908aae88f0bb9c66f11136fd315f8b70893c
Tyler Harms <tyler.harms@gmail.com>
2013-10-02 16:52:29 -0500

    New mailer to: url

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,6 +8,6 @@ class ContactMailer < ActionMailer::Base
 
   def submission_email(speaker_submission)
     @speaker_submission = speaker_submission
-    mail(to: 'tyler.harms@gmail.com', subject: 'New Speaker Submission')
+    mail(to: 'hello@southdakotacodecamp.net', subject: 'New Speaker Submission')
   end
 end
