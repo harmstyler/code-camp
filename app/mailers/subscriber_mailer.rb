52a437cba7923f965ee37c1a17b6724dd3bf156b
Tyler Harms <tyler.harms@gmail.com>
2013-10-22 23:29:35 -0500

    limit sessions

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -2,7 +2,7 @@ class SubscriberMailer < ActionMailer::Base
   default from: "hello@southdakotacodecamp.net"
 
   def thanks_email(subscriber)
-    @sessions = Session.where(speaker_id: Speaker.where(display: true)).order("created_at DESC")
+    @sessions = Session.where(speaker_id: Speaker.where(display: true)).order("created_at DESC").limit(5)
     @subscriber = subscriber
     mail(to: @subscriber.email, subject: 'Thank you for subscribing')
   end
